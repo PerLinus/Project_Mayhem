49f39cdb7c78da729586be541e5124671dc43961
DESKTOP-N5DA8BF\Miroch <mikael@hallberg.com>
2017-02-09 15:42:47 +0100

    Ändrat primaryStageTitle till "Nackademiska Auktionsfrämjandet"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Nackademiska Auktonsfrämjandet");
+        primaryStage.setTitle("Nackademiska Auktionsfrämjandet");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
