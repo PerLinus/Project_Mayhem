1f71cc9a8115df211e05084ec38014d6a564feb3
Markus Averheim <MacsMac@Markuss-MacBook-Pro.local>
2017-02-13 15:07:31 +0100

    Kopplade några text feilds till  controllerklasser

--- src/controllers/AddAuctionController.java
+++ src/controllers/AddAuctionController.java
@@ -1,4 +1,13 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class AddAuctionController {
+
+    @FXML
+    private TextField txfStartDate, txfEndDate, txfStartPrice, txfAcceptPrice;
+    @FXML
+    private ComboBox cbChooseProduct;
 }
