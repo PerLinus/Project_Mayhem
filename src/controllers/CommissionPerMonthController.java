9564376075f191e4ab18b2af4cd4a852b2b66e44
Markus Averheim <MacsMac@Markuss-MacBook-Pro.local>
2017-02-13 17:58:16 +0100

    Jobbar på TW listor

--- src/controllers/CommissionPerMonthController.java
+++ src/controllers/CommissionPerMonthController.java
@@ -1,15 +1,76 @@
 package controllers;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import models.CommissionPerMonth;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommissionPerMonthController {
 
     @FXML
-    private TableView twYear, twMonth, twCommission;
+    private TableColumn tcYear, tcMonth, tcCommission;
+    @FXML
+    private TableView twCommissionPerMonth;
 
-    public void initialize(){
+    private List<String> yearList = new ArrayList<>();
+    private List<String> monthList = new ArrayList<>();
+    private List<Double> commissionList = new ArrayList<>();
+    private List<CommissionPerMonth> commissionPerMonthList = new ArrayList<>();
+
+    public void initialize() {
         loadAuctionList();
+        ObservableList<String> observableYearList = FXCollections.observableArrayList(yearList);
+        ObservableList<String> observableMonthList = FXCollections.observableArrayList(monthList);
+        ObservableList<Double> observableCommissionList = FXCollections.observableArrayList(commissionList);
+
+        for (int i = 0 ; i < yearList.size() ; i++){
+            tcYear.setText(yearList.get(i));
+        }
+        for (int i = 0 ; i < monthList.size() ; i++){
+            tcMonth.setText(monthList.get(i));
+        }
+        for (int i = 0 ; i < commissionList.size() ; i++){
+            tcCommission.setText(commissionList.get(i).toString());
+        }
+/**
+            tcYear.setCellValueFactory(new PropertyValueFactory<CommissionPerMonth, String>("year"));
+            tcMonth.setCellFactory(new PropertyValueFactory<CommissionPerMonth, String>("month"));
+            tcCommission.setCellValueFactory(new PropertyValueFactory<CommissionPerMonth, Double>("Commission"));
+
+            twCommissionPerMonth.getItems().setAll(commissionPerMonthList);
+ */
+
+    }
+
+
+    private void loadAuctionList() {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project_Mayhem?useSSL=false", "root", "root")) {
+
+            try (Statement statement = connection.createStatement()) {
+
+                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM Commission_Per_Month_View")) {
+
+                    while (resultSet.next()) {
+                        String year = resultSet.getString("Year");
+                        String month = resultSet.getString("Month");
+                        double commission = resultSet.getDouble("Commission");
+
+                        yearList.add(year);
+                        monthList.add(month);
+                        commissionList.add(commission);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/models/CommissionPerMonth.java
@@ -0,0 +1,41 @@
+package models;
+
+/**
+ * Created by MacsMac on 2017-02-13.
+ */
+public class CommissionPerMonth {
+
+    private String year;
+    private String month;
+    private double commission;
+
+    public CommissionPerMonth(String year, String month, double commission) {
+        this.year = year;
+        this.month = month;
+        this.commission = commission;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+}
