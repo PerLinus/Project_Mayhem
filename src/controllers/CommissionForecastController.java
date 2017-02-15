4ba3460ff41c4ffb9d6a29c006b173ded2d010b3
Markus Averheim <MacsMac@Markuss-MacBook-Pro.local>
2017-02-15 12:48:14 +0100

    Fixade datepicker till forecast

--- src/controllers/CommissionForecastController.java
+++ src/controllers/CommissionForecastController.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -12,6 +13,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import models.CommissionForecast;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +21,13 @@ public class CommissionForecastController {
 
     List<CommissionForecast> commissionForecastList = new ArrayList<>();
 
+    /**
     @FXML
     private TextField txfFromDate, txfToDate;
+    */
+
+    @FXML
+    private DatePicker dpFromDate, dpToDate;
 
     @FXML
     private TableView twCommissionForecast;
@@ -31,16 +38,15 @@ public class CommissionForecastController {
 
     public void onClickGetForecast(ActionEvent actionEvent) {
 
-        String startDate = txfFromDate.getText();
-        String endDate = txfToDate.getText();
-
+        LocalDate startDate = dpFromDate.getValue();
+        LocalDate endDate = dpToDate.getValue();
 
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project_Mayhem?useSSL=false", "root", "root")) {
 
             try (CallableStatement cstm = connection.prepareCall("CALL Est_auction_report(?,?)")) {
 
-                cstm.setString(1, startDate);
-                cstm.setString(2, endDate);
+                cstm.setDate(1, Date.valueOf(startDate));
+                cstm.setDate(2, Date.valueOf(endDate));
 
                 ResultSet result = cstm.executeQuery();
 
