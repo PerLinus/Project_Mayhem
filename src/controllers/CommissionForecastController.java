893eb2184f53d22d336c632d136659632e95fc68
SimonPereZ <perez.lencinas@gmail.com>
2017-02-15 14:25:27 +0100

    clear list in CommissionForecast

--- src/controllers/CommissionForecastController.java
+++ src/controllers/CommissionForecastController.java
@@ -33,6 +33,8 @@ public class CommissionForecastController {
 
     public void onClickGetForecast(ActionEvent actionEvent) {
 
+        commissionForecastList.clear();
+
         LocalDate startDate = dpFromDate.getValue();
         LocalDate endDate = dpToDate.getValue();
 
